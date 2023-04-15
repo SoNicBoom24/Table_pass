25fb76736e53f2850b579c954f284137cfc11e05
SoNicBoom24 <boomzahrk@gmail.com>
2023-04-15 10:03:47 +0700

    feat: Navbar

--- src/pages/app/table.js
+++ src/pages/app/table.js
@@ -1,9 +1,7 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import { useRouter } from "next/router";
 import { Button } from "@chakra-ui/react";
+import Navbar from "../components/navbar";
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
@@ -12,11 +10,14 @@ export default function Home() {
 
 
   return (
-    <main className="flex flex-col justify-center items-center w-full h-screen bg-[#1A1B1B]">
-      <div>
-        <p className='text-5xl text-[#A47C37] pb-4'>My Table</p>
-        <Button colorScheme="yellow" size="lg" onClick={backToHome} w='full'>Back Home</Button>
-      </div>
-    </main>
+    <>
+      <Navbar />
+      <main className="flex flex-col justify-center items-center w-full h-screen bg-[#1A1B1B]">
+        <div>
+          <p className='text-5xl text-[#A47C37] pb-4'>My Table</p>
+          <Button colorScheme="yellow" size="lg" onClick={backToHome} w='full'>Back Home</Button>
+        </div>
+      </main>
+    </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import { Button } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { MdTableRestaurant } from "react-icons/md";
+import Navbar from "./components/navbar";
 
-// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
@@ -12,13 +10,15 @@ export default function Home() {
   const navigation = (url) => router.push(url);
 
   return (
-    <main className="flex flex-col justify-center items-center w-full h-screen bg-[#1A1B1B]">
-      <div>
-        <p className='text-[#A47C37] text-5xl pb-3 flex items-end'>Table Pass<MdTableRestaurant color='pink' size='40'/></p>
+    <>
+      <Navbar />
+      <main className="flex flex-col justify-center items-center w-full">
+        <div>
+          <p className='text-[#A47C37] text-5xl pb-3 flex items-end'>Table Pass<MdTableRestaurant color='black' size='40' /></p>
 
-        <Button colorScheme="yellow" size="lg" onClick={() => navigation('/app/table')} w='full'>Join Table</Button>
-      </div>
-    </main>
-    
+          <Button colorScheme="yellow" size="lg" onClick={() => navigation('/app/table')} w='full'>Join Table</Button>
+        </div>
+      </main>
+    </>
   )
 }
