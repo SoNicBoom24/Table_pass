c41cd1177e6fd24a6bd945139d9c85a3cb0e5136
SoNicBoom24 <boomzahrk@gmail.com>
2023-04-15 22:23:26 +0700

    feat: home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Input} from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import { MdTableRestaurant } from "react-icons/md";
+import { SiClockify } from "react-icons/si";
 import Navbar from "./components/navbar";
+import Image from 'next/image'
+
+import Card_Food from "./components/cardfood";
 
 
 export default function Home() {
@@ -12,12 +15,35 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="flex flex-col justify-center items-center w-full">
-        <div>
-          <p className='text-[#A47C37] text-5xl pb-3 flex items-end'>Table Pass<MdTableRestaurant color='black' size='40' /></p>
+      <main className="flex flex-col justify-center items-center w-full gap-14 mb-5">
+        <>
+          <div className="grid grid-cols-6 gap-2 w-full justify-center items-center h-96">
+            <div className="col-start-2 col-span-2">
+              <p className='text-5xl pb-10 flex justify-center items-end'>The Meal With Queueless</p>
+              <div className="flex border-2 p-1">
+                <Input placeholder='ระบุชื่อร้าน' fontWeight='bold' textAlign='center' size='lg' />
+                <Button size='lg' colorScheme="" className="bg-black ml-4">ค้นหา</Button>
+              </div>
+            </div>
 
-          <Button colorScheme="yellow" size="lg" onClick={() => navigation('/app/table')} w='full'>Join Table</Button>
-        </div>
+            <div className="grid col-span-2 col-end-7 place-items-end">
+              <Image src="/images/home.jpg" width={700} height={500} sizes="(max-width: 1000px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw" />
+            </div>
+          </div>
+        </>
+
+        <>
+          <div className="grid grid-cols-6 gap-10 w-full justify-center items-center">
+            <div className="col-start-2 col-span-2">
+              <p className='text-4xl flex items-center'>ร้านอาหารที่ร่วมรายการ <SiClockify className="ml-3" /></p>
+            </div>
+            <Card_Food />
+            <Card_Food />
+            <Card_Food />
+          </div>
+        </>
       </main>
     </>
   )
