5b872a10b2571c45c139999f1cead50cef72e736
ter2002 <ter2002@hotmail.co.th>
2023-04-16 20:10:39 +0700

    fix: default value and size button

--- src/pages/app/buyticket.js
+++ src/pages/app/buyticket.js
@@ -1,7 +1,18 @@
 import { useRouter } from "next/router";
-import { Button, useNumberInput, HStack, Input, Card, CardBody, CardFooter, Text, Heading, Stack } from "@chakra-ui/react";
+import {
+  Button,
+  useNumberInput,
+  HStack,
+  Input,
+  Card,
+  CardBody,
+  CardFooter,
+  Text,
+  Heading,
+  Stack,
+} from "@chakra-ui/react";
 import Navbar from "../components/navbar";
-import Image from 'next/image';
+import Image from "next/image";
 import { HiOutlineMail } from "react-icons/hi";
 import { HiOutlineTicket } from "react-icons/hi2";
 import { IoLocationOutline } from "react-icons/io5";
@@ -9,10 +20,9 @@ import { FiPhone } from "react-icons/fi";
 
 import Confirmbuy from "../app/confirmbuy";
 
-import { useSearchParams, Routes, Route, useParams } from "react-router-dom";
+// import { useSearchParams, Routes, Route, useParams } from "react-router-dom";
 
 export default function BuyTicket() {
-
   const router = useRouter();
   const navigation = (url) => {
     router.push(url);
@@ -21,53 +31,67 @@ export default function BuyTicket() {
   const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } =
     useNumberInput({
       step: 1,
-      defaultValue: 0,
-      min: 0,
+      defaultValue: 1,
+      min: 1,
       max: 10,
       precision: 0,
-    })
+    });
 
-  const inc = getIncrementButtonProps()
-  const dec = getDecrementButtonProps()
-  const input = getInputProps()
+  const inc = getIncrementButtonProps();
+  const dec = getDecrementButtonProps();
+  const input = getInputProps();
 
-  console.log(input.value)
+  console.log(input.value);
   return (
     <>
       <Navbar />
       <main className="flex flex-col justify-center items-center w-full gap-10 mb-5 mt-9">
         <div className="grid grid-cols-6 gap-2 w-full justify-center items-center">
-          <Card className="col-start-2 col-span-4"
-            direction={{ base: 'column', sm: 'row' }}
-            overflow='hidden'
-            variant='outline'
+          <Card
+            className="col-start-2 col-span-4"
+            direction={{ base: "column", sm: "row" }}
+            overflow="hidden"
+            variant="outline"
           >
             <Image
               alt="Food"
-              maxW={{ base: '100%', sm: '200px' }}
-              src='/images/food.jpg'
-              width={450} height={200}
+              maxW={{ base: "100%", sm: "200px" }}
+              src="/images/food.jpg"
+              width={450}
+              height={200}
             />
 
             <Stack>
               <CardBody className="ml-10">
-                <Heading size='lg'>ร้านไอสลัดผัก</Heading>
+                <Heading size="lg">ร้านไอสลัดผัก</Heading>
 
-                <Text py="2" className="flex items-center text-xl my-2"><IoLocationOutline className="mr-2 text-3xl" />: ข้างในใจเธอ</Text>
-                <Text className="flex items-center text-xl mb-3"><HiOutlineTicket className="mr-2 text-3xl" />: 450 บาท/1 คน</Text>
+                <Text py="2" className="flex items-center text-xl my-2">
+                  <IoLocationOutline className="mr-2 text-3xl" />: ข้างในใจเธอ
+                </Text>
+                <Text className="flex items-center text-xl mb-3">
+                  <HiOutlineTicket className="mr-2 text-3xl" />: 450 บาท/1 คน
+                </Text>
 
-                <HStack maxW='200px'>
+                <HStack maxW="200px">
                   <Button {...dec}>-</Button>
-                  <Input {...input} />
+                  <Input className="text-center" {...input} />
                   <Button {...inc}>+</Button>
                 </HStack>
-                <Text className="flex justify-center items-center text-xl mt-3">ราคารวม {input.value * 450} บาท</Text>
-
+                <Text className="flex justify-center items-center text-xl mt-3">
+                  ราคารวม {input.value * 450} บาท
+                </Text>
               </CardBody>
 
               <CardFooter>
-                <Button variant='solid' colorScheme='' className="bg-black ml-10"
-                  onClick={() => navigation('/app/confirmbuy')} >
+                <Button
+                  variant="solid"
+                  colorScheme=""
+                  className="bg-black ml-10 -mt-8"
+                  size="lg"
+                  height="50px"
+                  width="200px"
+                  onClick={() => navigation("/app/confirmbuy")}
+                >
                   Buy Ticket
                 </Button>
               </CardFooter>
@@ -77,7 +101,7 @@ export default function BuyTicket() {
 
         <div className="grid grid-cols-6 gap-2 w-full justify-center items-center">
           <div className="col-start-2 col-span-3 bg-slate-200 rounded p-3">
-            <p className='flex text-3xl ml-3'>รายระเอียดบัตร</p>
+            <p className="flex text-3xl ml-3">รายระเอียดบัตร</p>
           </div>
         </div>
 
@@ -87,16 +111,27 @@ export default function BuyTicket() {
               <li>บัตรมีอายุการใช้งานได้ 60 วัน</li>
               <li>ร้านเปิด ทุกวัน ตั้งแต่ 08.00 - 18.00 น.</li>
               <li>ลูกค้าสามารถเข้ามารับประทานอาหารได้ทุกช่วงเวลาที่ร้านเปิด</li>
-              <li>หากโต๊ะมีการใช้งานอยู่ ลูกค้าสามารถสแกน QR Code หน้าร้านเพื่อรอรับประทาน และหากถึงคิวของท่าน พนักงานจะรีบติดต่อกลับโดยเร็วที่สุด</li>
+              <li>
+                หากโต๊ะมีการใช้งานอยู่ ลูกค้าสามารถสแกน QR Code
+                หน้าร้านเพื่อรอรับประทาน และหากถึงคิวของท่าน
+                พนักงานจะรีบติดต่อกลับโดยเร็วที่สุด
+              </li>
             </ol>
           </div>
           <div className="col-start-2 col-span-3 mt-5">
-            <p className='flex text-2xl mb-3 underline underline-offset-2'>ช่องทางการติดต่อทางร้าน</p>
-            <p className='flex text-2xl items-center'><FiPhone className="mr-2" size={30} /> Phone : 081-799-6452</p>
-            <p className='flex text-2xl items-center'><HiOutlineMail className="mr-2" size={30} /> Email : BoomInwZa55@gmail.com</p>
+            <p className="flex text-2xl mb-3 underline underline-offset-2">
+              ช่องทางการติดต่อทางร้าน
+            </p>
+            <p className="flex text-2xl items-center">
+              <FiPhone className="mr-2" size={30} /> Phone : 081-799-6452
+            </p>
+            <p className="flex text-2xl items-center">
+              <HiOutlineMail className="mr-2" size={30} /> Email :
+              BoomInwZa55@gmail.com
+            </p>
           </div>
         </div>
       </main>
     </>
-  )
+  );
 }
