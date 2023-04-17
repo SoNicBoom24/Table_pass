cfcb6e38b2186e09431820007fad9104e9d89b1c
SoNicBoom24 <boomzahrk@gmail.com>
2023-04-17 11:08:32 +0700

    feat: login google

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,18 @@
 import * as React from 'react'
 import '@/styles/globals.css'
 import { ChakraProvider } from "@chakra-ui/react";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export default function App({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <GoogleOAuthProvider clientId='294858680175-sksp0ktcs6md52n5vs1rp2q9mcec873i.apps.googleusercontent.com' >
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </GoogleOAuthProvider>
   )
 }
+
+
+// Client ID       294858680175-sksp0ktcs6md52n5vs1rp2q9mcec873i.apps.googleusercontent.com
+// Client secret   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
--- src/pages/components/navbar.js
+++ src/pages/components/navbar.js
@@ -6,80 +6,86 @@ import { BsFillPersonFill } from "react-icons/bs";
 import { FcGoogle } from "react-icons/fc";
 import { useRouter } from "next/router";
 import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  useDisclosure,
-  FormControl,
-  FormLabel,
-  Input,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+    FormControl,
+    FormLabel,
+    Input,
 } from "@chakra-ui/react";
 
+import { GoogleLogin } from '@react-oauth/google';
+
 export default function My_Navbar() {
-  const router = useRouter();
-  const home = () => router.replace("/");
-  const { isOpen, onOpen, onClose } = useDisclosure();
+    const router = useRouter();
+    const home = () => router.replace("/");
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const initialRef = React.useRef(null);
-  const finalRef = React.useRef(null);
+    const initialRef = React.useRef(null);
+    const finalRef = React.useRef(null);
 
-  return (
-    <>
-      <Navbar className="sticky inset-0 z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-8 lg:py-4 border-b-2 bg-white drop-shadow-lg">
-        <div className="flex items-center justify-between">
-          <Button className="flex cursor-pointer" onClick={home}>
-            {/* <Button className="flex cursor-pointer" onClick={() => navigation('/app/buyticket')}> */}
-            <GiSandsOfTime size="40" />
-            <Typography className="mr-4  py-1.5 font-medium text-xl">
-              Table Pass
-            </Typography>
-          </Button>
-          <div className="flex items-center gap-2">
-            <Button
-              className="flex font-normal pl-1 mr-4 items-center"
-              onClick={onOpen}
-            >
-              <BsFillPersonFill className=" mr-1" size="30" /> Log In
-            </Button>
-            <Button
-              className="items-center"
-              onClick={() => navigation("/app/ticket")}
-            >
-              <HiOutlineTicket size="35" className="pl-1" />
-            </Button>
-          </div>
-        </div>
-      </Navbar>
+    const responseMessage = (response) => {
+        console.log(response);
+    };
+    const errorMessage = (error) => {
+        console.log(error);
+    };
 
-      <Modal
-        initialFocusRef={initialRef}
-        finalFocusRef={finalRef}
-        isOpen={isOpen}
-        onClose={onClose}
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Login with your Google account</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>
-            <Button
-              size="lg"
-              variant="outlined"
-              color="green"
-              //   color="blue-gray"
-              className="flex items-center gap-3 rounded border-4 bg-neutral-50 p-1"
-              fullWidth
+    return (
+        <>
+            <Navbar className="sticky inset-0 z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-8 lg:py-4 border-b-2 bg-white drop-shadow-lg">
+                <div className="flex items-center justify-between">
+                    <Button className="flex cursor-pointer" onClick={home}>
+                        {/* <Button className="flex cursor-pointer" onClick={() => navigation('/app/buyticket')}> */}
+                        <GiSandsOfTime size="40" />
+                        <Typography className="mr-4  py-1.5 font-medium text-xl">
+                            Table Pass
+                        </Typography>
+                    </Button>
+                    <div className="flex items-center gap-2">
+                        <Button
+                            className="flex font-normal pl-1 mr-4 items-center"
+                            onClick={onOpen}
+                        >
+                            <BsFillPersonFill className=" mr-1" size="30" /> Log In
+                        </Button>
+                        <Button
+                            className="items-center"
+                            onClick={() => navigation("/app/ticket")}
+                        >
+                            <HiOutlineTicket size="35" className="pl-1" />
+                        </Button>
+                    </div>
+                </div>
+            </Navbar>
+
+            <Modal
+                initialFocusRef={initialRef}
+                finalFocusRef={finalRef}
+                isOpen={isOpen}
+                onClose={onClose}
             >
-              <FcGoogle size="60" className="pl-1" />
-              Login with Google
-            </Button>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-    </>
-  );
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Login with your Google account</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody pb={6}>
+                        <GoogleLogin onSuccess={responseMessage} onError={errorMessage}
+                            size="large"
+                            width="400"
+                            type="standard"
+                            theme="outline"
+                            text="signin_with"
+                        >
+                        </GoogleLogin>
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
+        </>
+    );
 }
