ef071f888efb1d480c313dc2285428d92caa4cff
SoNicBoom24 <boomzahrk@gmail.com>
2023-04-17 07:48:11 +0700

    del: card food don't use some code

--- src/pages/components/cardfood.js
+++ src/pages/components/cardfood.js
@@ -42,32 +42,6 @@ export default function Card_Food(props) {
           </CardBody>
         </LinkOverlay>
       </Card>
-
-      {/* <Card
-        className="col-start-4 col-span-2 p-3 cursor-pointer drop-shadow-lg"
-        _hover={{ bg: "gray.200" }}
-      >
-        <LinkOverlay onClick={() => navigation("/app/buyticket")}>
-          <CardBody>
-            <Image
-              alt="food"
-              src="/images/food.jpg"
-              width={600}
-              height={400}
-              className="rounded-xl"
-            />
-            <Heading size="lg" className="my-2">
-              ร้านไอสลัดผัก
-            </Heading>
-            <Text className="flex items-center text-xl mb-2">
-              <IoLocationOutline className="mr-2 text-3xl" />: ข้างในใจเธอ
-            </Text>
-            <Text className="flex items-center text-xl">
-              <HiOutlineTicket className="mr-2 text-3xl" />: 450 บาท/1 คน
-            </Text>
-          </CardBody>
-        </LinkOverlay>
-      </Card> */}
     </>
   );
 }
